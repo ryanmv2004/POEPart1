d58ef051f7fd79fb6ae359a88b4930a487520bef
ryanmv2304 <ST10365052@vcconnect.edu.za>
2024-04-12 11:08:06 +0200

    Added an ingredients class to store all the attributes that the user would need to input about the recipie.

---
+++ POEPart1/Ingredients.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POEPart1
+{
+    internal class Ingredients
+    {
+        private String inName { get; set; }
+        private String inQuantity { get; set; }
+        private String inUnit { get; set; }
+
+    }
+}
