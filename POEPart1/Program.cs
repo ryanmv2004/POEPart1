ce35a2374416dba2be64fd292ea7609f2f06c0a2
ryanmv2304 <ST10365052@vcconnect.edu.za>
2024-04-12 11:41:01 +0200

    Added limited functionality to the main class by adding a display screen for each choice and making sure that the exit functionality works.

--- POEPart1/Program.cs
+++ POEPart1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +10,45 @@ namespace POEPart1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Recipie Storage App!");
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine();
-            Console.WriteLine("Please make a Selection on which tool to use:");
-            Console.WriteLine("1) Enter Recipie");
-            Console.WriteLine("2) Display Recipie");
-            Console.WriteLine("3) Scale Recipie");
-            Console.WriteLine("4) Clear all Recipies");
-            Console.WriteLine("------------------------------------");
-            Console.WriteLine("Please make a choice by entering only the tool number:");
-            int choice = Int32.Parse(Console.ReadLine());
-            Console.WriteLine(choice);
-            Console.ReadKey();
+            Boolean SENTINAL = true;
+            while (SENTINAL)
+            {
+
+
+                Console.WriteLine("Welcome to the Recipie Storage App!");
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine();
+                Console.WriteLine("Please make a Selection on which tool to use:");
+                Console.WriteLine("1) Enter Recipie");
+                Console.WriteLine("2) Display Recipie");
+                Console.WriteLine("3) Scale Recipie");
+                Console.WriteLine("4) Clear all Recipies");
+                Console.WriteLine("5) Exit");
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("Please make a choice by entering only the tool number:");
+                int choice = Int32.Parse(Console.ReadLine());
+                if (choice == 1) 
+                {
+                    Console.WriteLine("1");
+                }
+                if (choice == 2) 
+                {
+                    Console.WriteLine("2");
+                }
+                if (choice == 3)
+                {
+                    Console.WriteLine("3");
+                }
+                if (choice == 4) 
+                {
+                    Console.WriteLine("4");
+                }
+                if (choice == 5) 
+                {
+                    SENTINAL = false;
+                    break;
+                }
+            }
         }
     }
 }
